b06bd1d25583225a70251cc07eee357533773e20
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-03-05 20:31:46 +0530

    Added Input & Output example to 175. Combine Two Tables

--- Database-Problems/175_Combine_Two_Tables.sql
+++ Database-Problems/175_Combine_Two_Tables.sql
@@ -6,3 +6,11 @@ https://leetcode.com/problems/combine-two-tables/
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p LEFT JOIN Address a
 ON p.personId = a.personId
+
+/*
+Input:
+{"headers":{"Person":["personId","lastName","firstName"],"Address":["addressId","personId","city","state"]},"rows":{"Person":[[1,"Wang","Allen"],[2,"Alice","Bob"]],"Address":[[1,2,"New York City","New York"],[2,3,"Leetcode","California"]]}}
+
+Output:
+{"headers": ["firstName", "lastName", "city", "state"], "values": [["Allen", "Wang", null, null], ["Bob", "Alice", "New York City", "New York"]]}
+*/
