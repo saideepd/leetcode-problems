f34d730cf9280c57f61c8df843b3cf366cd97e6a
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-03-30 20:26:30 +0530

    Added comments & updated some logic for 34. Find First & Last Position of Element

--- 34FindFirstAndLastPositionOfElementInSortedArray.js
+++ 34FindFirstAndLastPositionOfElementInSortedArray.js
@@ -11,11 +11,12 @@
 var searchRange = function (nums, target) {
     let ans = [-1, -1];
 
-    let start = search(nums, target, true);
-    let end = search(nums, target, false);
-
-    ans[0] = start;
-    ans[1] = end;
+    // Find first & last index value of target
+    // And set the ans array
+    ans[0] = search(nums, target, true);
+    // Try finding last index only if start is not -1 which implies the target does not exist in array
+    if (ans[0] != -1)
+        ans[1] = search(nums, target, false);
 
     return ans;
 }
@@ -24,16 +25,22 @@ const search = (nums, target, findStartIndex) => {
     let start = 0, end = nums.length - 1, ans = -1;
 
     while (start <= end) {
+        // Calculate mid value
         let mid = parseInt(start + (end - start) / 2);
 
+        // If target is less than mid element, update end pointer
         if (target < nums[mid])
             end = mid - 1;
+        // If target is greater than mid element, update start pointer
         else if (target > nums[mid])
             start = mid + 1;
+        // If mid element matches target value, set the ans to mid value
         else {
             ans = mid;
+            // If we want to find the start index, update the end pointer
             if (findStartIndex)
                 end = mid - 1;
+            // If we want to find the end index, update the start pointer
             else
                 start = mid + 1;
         }
