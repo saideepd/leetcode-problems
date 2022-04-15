438aa25bbc5907b7c0dcc945ee7a517caf207c96
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-04-16 01:32:15 +0530

    Updated logic to sum for 268. Missing Number

--- 268MissingNumber.js
+++ 268MissingNumber.js
@@ -7,18 +7,21 @@
  * @output number
  */
 var missingNumber = function (nums) {
-    let n = nums.length;
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i <= n; i++) {
-        if (i !== nums[i])
-            return i;
-    }
+    // let n = nums.length;
+    // nums.sort((a, b) => a - b);
+    // for (let i = 0; i <= n; i++) {
+    //     if (i !== nums[i])
+    //         return i;
+    // }
 
     // // Another Approach using sum of n numbers formula
-    // let n = nums.length;
-    // let sum1 = (n * (n + 1)) / 2;
+    let n = nums.length;
+    let sum1 = (n * (n + 1)) / 2;
     // let sum2 = nums.reduce((total, next) => total + next);
-    // return sum1 - sum2;
+    let sum2 = 0;
+    for (let num of nums)
+        sum2 += num;
+    return sum1 - sum2;
 }
 
 console.log('Missing Number');
